f54cac04d5458a855b8047151d63ac96c1b4fe40
Stephen Allen <staylorallen@gmail.com>
2019-07-22 22:25:11 -0400

    Stable -- Working gems, random number, total score incrementer

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,37 @@
 window.onload = function() {
 
+  let totalScore = 0;
+
+  let gem_blue = Math.floor(Math.random() * 12 + 1);
+  let gem_green = Math.floor(Math.random() * 12 + 1);
+  let gem_yellow = Math.floor(Math.random() * 12 + 1);
+  let gem_red = Math.floor(Math.random() * 12 + 1);
 
 
-  function generateRandomNumber(){
-    //get a number between 1 and 40
-    return Math.floor(Math.random()*40+1);
+
+  function initGame() {
+    let randomNumb = Math.floor(Math.random() * 30 + 20);
+    document.querySelector(".randomNumber").innerHTML = randomNumb;
+    document.querySelector(".totalScore-Counter").innerHTML = totalScore;
+
   }
 
+  document.querySelector(".wrapper-crystals").addEventListener('click', function(e) {
+    console.log(e.target.alt);
+    if (e.target.alt == 'gem-blue') {
+      totalScore += gem_blue;
+      document.querySelector(".totalScore-Counter").innerHTML = totalScore;
+    } else if (e.target.alt == 'gem-green') {
+      totalScore += gem_green;
+      document.querySelector(".totalScore-Counter").innerHTML = totalScore;
+    } else if (e.target.alt == 'gem-yellow') {
+      totalScore += gem_yellow;
+      document.querySelector(".totalScore-Counter").innerHTML = totalScore;
+    } else if (e.target.alt == 'gem-red') {
+      totalScore += gem_red;
+      document.querySelector(".totalScore-Counter").innerHTML = totalScore;
+    }
+  });
 
-  let randomNumb = generateRandomNumber();
-  document.querySelector(".randomNumber").innerHTML = randomNumb;
-
+  initGame();
 };
