670c43abb681e4ae215605ef6d60b1978b97a4aa
Stephen Allen <staylorallen@gmail.com>
2019-07-22 21:48:36 -0400

    generate number randomly + add number to DOM

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
-() => {
+window.onload = function() {
 
-}
+
+
+  function generateRandomNumber(){
+    //get a number between 1 and 40
+    return Math.floor(Math.random()*40+1);
+  }
+
+
+  let randomNumb = generateRandomNumber();
+  document.querySelector(".randomNumber").innerHTML = randomNumb;
+
+};
